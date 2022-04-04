cf053d3f8c7807b32fc45655290ddea19830a1b0
CepotOi <rizkihutama69@gmail.com>
2022-04-04 20:39:44 +0700

    using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,42 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: { allPostsData }
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section classname={utilStyles.headingMd}>
+      <section className={utilStyles.headingMd}>
         <p>My name is Rizki Hutama</p>
-        <p>I am a software engineer</p>
+        <p>I am a junior web developer</p>
       </section>
-    </Layout>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              <small>{id}</small>
+              <br />
+              <time>{date}</time>
+            </li>
+          ))}
+
+        </ul>
+      </section >
+    </Layout >
   );
 }
